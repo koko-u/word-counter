4d9b527e87a9126ebc2f78f473d8efc4d74974aa
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2023-06-14 21:28:54 +0900

    Add a description of the command line arguments

--- src/opts.rs
+++ src/opts.rs
@@ -3,5 +3,7 @@ use std::path;
 #[derive(Debug, clap::Parser)]
 #[command(author, version, about)]
 pub struct Opt {
+    /// File(s) to be counted for words
+    #[arg(name = "FILE")]
     pub files: Vec<path::PathBuf>,
 }
